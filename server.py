04260c0247aeed7f8c3b1cf4d5725378de6cca0c
Vincent Velociter <vincent.velociter@gmail.com>
2014-01-12 01:03:51 +0100

    work

--- server.py
+++ server.py
@@ -1,23 +1,24 @@
 import requests
 import re
 
+SERVER_HOST = 'http://localhost:9000'
 
-resp = requests.get('http://localhost:9000/api/training/alone')
+TAVERN = 0
+AIR = -1
+WALL = -2
+
+resp = requests.get(SERVER_HOST + '/api/training/alone')
 json = resp.json()
 
 strTiles = json['game']['board']['tiles']
 size = json['game']['board']['size']
 tiles = [string[i:i+2] for i in range(0, len(string), 2)]
 
-TAVERN = 0
-AIR = -1
-WALL = -2
+# board is a two dimension vector
+board = [tiles[i:i+size] for i in range(0, len(tiles), size)]
 
-def parseTile(str):
-    if ('  ' == str):
-        return AIR
-    if ('##' == str):
-        return WALL
-    if ('[]' == str):
-        return TAVERN
+
+class Board:
+    def __init__(self, tiles):
+        self.tiles = tiles
 
