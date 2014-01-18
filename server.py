23beb5d7788ef27c02174c71cf810286d361e413
Vincent Jousse <vincent@jousse.org>
2014-01-18 19:53:09 +0100

    Pythonize variable names

--- server.py
+++ server.py
@@ -6,10 +6,10 @@ import requests
 import re
 from bot import RandomBot
 
-def get_new_game_state(serverHost, key, numberOfTurns = '20', mode='training'):
+def get_new_game_state(server_host, key, number_of_turns = '20', mode='training'):
     if(mode=='training'):
-        params = { 'key': key, 'turns': numberOfTurns}
-        r = requests.post(serverHost + '/api/training', params)
+        params = { 'key': key, 'turns': number_of_turns}
+        r = requests.post(server_host + '/api/training', params)
 
         if(r.status_code == 200):
             return r.json()
@@ -23,26 +23,26 @@ def move(url, direction):
     r = requests.post(url, {'dir': direction})
     return r.json()
 
-def start(serverHost, key, bot, numberOfGames = 20):
+def start(server_host, key, bot, number_of_games = 20):
 
-    def play(state, gamesPlayed = 0):
+    def play(state, games_played = 0):
         if (state['game']['finished']):
-            gamesPlayed += 1
-            print('Game finished: %d/%d' % (gamesPlayed, numberOfGames))
+            games_played += 1
+            print('Game finished: %d/%d' % (games_played, number_of_games))
 
-            if(gamesPlayed < numberOfGames):
+            if(games_played < number_of_games):
                 print('asking a new game')
-                state = get_new_game_state(serverHost, key)
-                play(state, gamesPlayed)
+                state = get_new_game_state(server_host, key)
+                play(state, games_played)
         else:
             url = state['playUrl']
             direction = bot.move(state)
-            newState = move(url, direction)
+            new_state = move(url, direction)
 
             print("Playing turn %d with direction %s" % (state['game']['turn'], direction))
-            play(newState, gamesPlayed)
+            play(new_state, games_played)
 
-    state = get_new_game_state(serverHost, key)
+    state = get_new_game_state(server_host, key)
     print("Start: " + state['viewUrl'])
     play(state)
 
