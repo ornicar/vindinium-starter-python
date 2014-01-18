781b4d43bc9c3d185a287ba335f58a7e069c083b
Vincent Jousse <vincent@jousse.org>
2014-01-18 19:50:41 +0100

    Add key and change training url

--- server.py
+++ server.py
@@ -6,9 +6,16 @@ import requests
 import re
 from bot import RandomBot
 
-def get_new_game_state(serverHost, mode='training'):
+def get_new_game_state(serverHost, key, numberOfTurns = '20', mode='training'):
     if(mode=='training'):
-        return requests.post(serverHost + '/api/training/alone').json()
+        params = { 'key': key, 'turns': numberOfTurns}
+        r = requests.post(serverHost + '/api/training', params)
+
+        if(r.status_code == 200):
+            return r.json()
+        else:
+            print("Error when creating the game")
+            print(r.text)
     else:
         pass
 
@@ -16,7 +23,7 @@ def move(url, direction):
     r = requests.post(url, {'dir': direction})
     return r.json()
 
-def start(serverHost, bot, numberOfGames = 20):
+def start(serverHost, key, bot, numberOfGames = 20):
 
     def play(state, gamesPlayed = 0):
         if (state['game']['finished']):
@@ -25,7 +32,7 @@ def start(serverHost, bot, numberOfGames = 20):
 
             if(gamesPlayed < numberOfGames):
                 print('asking a new game')
-                state = get_new_game_state(serverHost)
+                state = get_new_game_state(serverHost, key)
                 play(state, gamesPlayed)
         else:
             url = state['playUrl']
@@ -35,13 +42,13 @@ def start(serverHost, bot, numberOfGames = 20):
             print("Playing turn %d with direction %s" % (state['game']['turn'], direction))
             play(newState, gamesPlayed)
 
-    state = get_new_game_state(serverHost)
+    state = get_new_game_state(serverHost, key)
     print("Start: " + state['viewUrl'])
     play(state)
 
 if __name__ == "__main__":
-    if (len(sys.argv) > 2):
-        start(sys.argv[1], RandomBot(), int(sys.argv[2]))
+    if (len(sys.argv) > 3):
+        start(sys.argv[1], sys.argv[2], RandomBot(), int(sys.argv[3]))
     else:
-        print("Usage: %s <server> <number-of-games>" % (sys.argv[0]))
-        print('Example: %s http://localhost:9000 20' % (sys.argv[0]))
+        print("Usage: %s <server> <key> <number-of-games-to-play>" % (sys.argv[0]))
+        print('Example: %s http://localhost:9000 mySecretKey 20' % (sys.argv[0]))
