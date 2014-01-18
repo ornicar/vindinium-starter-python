e0f489dd635a5bbd788c9c72fee4ba8b34c4ff8f
Vincent Jousse <vincent@jousse.org>
2014-01-18 16:40:58 +0100

    Playing 20 games in a row

--- server.py
+++ server.py
@@ -16,18 +16,24 @@ def move(url, direction):
     r = requests.post(url, {'dir': direction})
     return r.json()
 
-def start(serverHost, bot):
+def start(serverHost, bot, numberOfGames = 20):
 
-    def play(state):
+    def play(state, gamesPlayed = 0):
         if (state['game']['finished']):
-            print('game finished')
+            gamesPlayed += 1
+            print('Game finished: %d/%d' % (gamesPlayed, numberOfGames))
+
+            if(gamesPlayed < numberOfGames):
+                print('asking a new game')
+                state = get_new_game_state(serverHost)
+                play(state, gamesPlayed)
         else:
             url = state['playUrl']
             direction = bot.move(state)
             newState = move(url, direction)
 
             print("Playing turn %d with direction %s" % (state['game']['turn'], direction))
-            play(newState)
+            play(newState, gamesPlayed)
 
     state = get_new_game_state(serverHost)
     print("Start: " + state['viewUrl'])
