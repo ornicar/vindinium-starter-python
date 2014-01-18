b01b96c93c02cb60c69440ce3e1a8e3d58dcd22a
Vincent Jousse <vincent@jousse.org>
2014-01-18 16:32:16 +0100

    Clean the code

--- server.py
+++ server.py
@@ -6,35 +6,35 @@ import requests
 import re
 from bot import RandomBot
 
-SERVER_HOST = 'http://localhost:9000'
-
-trainingState = requests.post(SERVER_HOST + '/api/training/alone').json()
-state = trainingState
-
-bot = RandomBot()
+def get_new_game_state(serverHost, mode='training'):
+    if(mode=='training'):
+        return requests.post(serverHost + '/api/training/alone').json()
+    else:
+        pass
 
 def move(url, direction):
     r = requests.post(url, {'dir': direction})
     return r.json()
 
-def start(server_url):
+def start(serverHost, bot):
+
     def play(state):
         if (state['game']['finished']):
             print('game finished')
         else:
             url = state['playUrl']
             direction = bot.move(state)
-            newState = move(state['playUrl'], direction)
+            newState = move(url, direction)
 
             print("Playing turn %d with direction %s" % (state['game']['turn'], direction))
             play(newState)
 
+    state = get_new_game_state(serverHost)
     print("Start: " + state['viewUrl'])
     play(state)
 
 if __name__ == "__main__":
     if (len(sys.argv) > 1):
-        SERVER_HOST = sys.argv[1]
-        start(sys.argv[1])
+        start(sys.argv[1], RandomBot())
     else:
         print('Specify the server, ex: "http://localhost:9000"')
