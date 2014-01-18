fe66dbe8d19e430c07270b1dc610526be2a27379
Vincent Jousse <vincent@jousse.org>
2014-01-18 20:10:41 +0100

    Add arena mode

--- server.py
+++ server.py
@@ -7,6 +7,8 @@ import re
 from bot import RandomBot
 
 def get_new_game_state(server_host, key, mode='training', number_of_turns = '20'):
+    """Get a JSON from the server containing the current state of the game"""
+
     if(mode=='training'):
         params = { 'key': key, 'turns': number_of_turns}
         r = requests.post(server_host + '/api/training', params)
@@ -16,16 +18,31 @@ def get_new_game_state(server_host, key, mode='training', number_of_turns = '20'
         else:
             print("Error when creating the game")
             print(r.text)
-    else:
-        pass
+    elif(mode=='arena'):
+        params = { 'key': key}
+        r = requests.post(server_host + '/api/arena', params)
+
+        if(r.status_code == 200):
+            return r.json()
+        else:
+            print("Error when creating the game")
+            print(r.text)
 
 def move(url, direction):
+    """Send a move to the server
+    
+    Moves can be one of: 'Stay', 'North', 'South', 'East', 'West' 
+    """
+
     r = requests.post(url, {'dir': direction})
     return r.json()
 
 def start(server_host, key, mode, bot, number_of_games = 20):
+    """Starts a game with all the required parameters"""
 
     def play(state, games_played = 0):
+        """Main game loop"""
+
         if (state['game']['finished']):
             games_played += 1
             print('Game finished: %d/%d' % (games_played, number_of_games))
@@ -51,4 +68,4 @@ if __name__ == "__main__":
         start(sys.argv[1], sys.argv[2], sys.argv[3], RandomBot(), int(sys.argv[4]))
     else:
         print("Usage: %s <server> <key> <[training|arena]> <number-of-games-to-play>" % (sys.argv[0]))
-        print('Example: %s http://localhost:9000 mySecretKey training 20' % (sys.argv[0]))
+        print('Example: %s http://vindinium.jousse.org mySecretKey training 20' % (sys.argv[0]))
