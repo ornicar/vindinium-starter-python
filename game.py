d3852689185fbd3a8048e16412d876a55b2e32fc
Vincent Velociter <vincent.velociter@gmail.com>
2014-01-18 18:34:03 +0100

    Remove bfs and related functions

--- game.py
+++ game.py
@@ -86,44 +86,6 @@ class Board:
 
         return (n_row, n_col)
 
-    def bfs(self, start):
-        ' Breadth first search generator function '
-        queue, visited = deque([(None, None, start)]), set([start])
-        while queue:
-            parent, direction, loc = queue.popleft()
-            yield parent, direction, loc
-            adjs = [(direction, self.to(loc, direction)) for direction in AIM.keys()]
-            for direction, adj_loc in adjs:
-                if self.passable(adj_loc) and adj_loc not in visited:
-                    visited.add(adj_loc)
-                    queue.append((loc, direction, adj_loc))
-
-    def find_closest(self, start, locs_set, depth=10000):
-        ' find closest in a set of locations and return searched loc and path '
-        paths = {None: []}
-        explored = 0
-        for parent, from_dir, child in self.bfs(start):
-            paths[child] = paths[parent] + [from_dir]
-            explored += 1
-            if child in locs_set:
-                return child, deque(paths[child][1:])
-            if depth and explored > depth:
-                return None, None
-        return None, None
-
-    def bfs_shortest_path(self, start, end, depth=10000):
-        ' return one of a possible shortest path '
-        paths = {None: []}
-        explored = 0
-        for parent, from_dir, child in self.bfs(start):
-            paths[child] = paths[parent] + [from_dir]
-            explored += 1
-            if child == end:
-                return deque(paths[child][1:])
-            if depth and explored > depth:
-                return None
-        return None
-
 
 
 class Hero:
@@ -132,15 +94,4 @@ class Hero:
         self.pos = hero['pos']
         self.life = hero['life']
         self.gold = hero['gold']
-
-
-def manhattan_distance(loc1, loc2):
-    'calculate the closest manhattan distance between two locations'
-    row1, col1 = loc1
-    row2, col2 = loc2
-    d_col = min(abs(col1 - col2), self.cols - abs(col1 - col2))
-    d_row = min(abs(row1 - row2), self.rows - abs(row1 - row2))
-    return d_row + d_col
-
-
 
