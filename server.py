7de7c719e4be0db111fc3bbcc2d6aee8b8ad6c65
Vincent Jousse <vincent@jousse.org>
2014-01-19 17:28:18 +0100

    Avoid too much recursion

--- server.py
+++ server.py
@@ -38,21 +38,13 @@ def move(url, direction):
     r = requests.post(url, {'dir': direction})
     return r.json()
 
-def start(server_host, key, mode, bot, number_of_games = 20):
+def start(server_host, key, mode, bot):
     """Starts a game with all the required parameters"""
 
     def play(state, games_played = 0):
         """Main game loop"""
 
-        if (state['game']['finished']):
-            games_played += 1
-            print('Game finished: %d/%d' % (games_played, number_of_games))
-
-            if(games_played < number_of_games):
-                print('asking a new game')
-                state = get_new_game_state(server_host, key, mode)
-                play(state, games_played)
-        else:
+        if (not state['game']['finished']):
             url = state['playUrl']
             direction = bot.move(state)
             new_state = move(url, direction)
@@ -61,7 +53,7 @@ def start(server_host, key, mode, bot, number_of_games = 20):
             play(new_state, games_played)
 
     if(mode=='arena'):
-        print u'Connected and waiting for other players to join…'
+        print(u'Connected and waiting for other players to join…')
 
     state = get_new_game_state(server_host, key, mode)
     print("Start: " + state['viewUrl'])
@@ -69,7 +61,10 @@ def start(server_host, key, mode, bot, number_of_games = 20):
 
 if __name__ == "__main__":
     if (len(sys.argv) > 4):
-        start(sys.argv[1], sys.argv[2], sys.argv[3], RandomBot(), int(sys.argv[4]))
+        number_of_games = int(sys.argv[4])
+        for i in range(number_of_games):
+            start(sys.argv[1], sys.argv[2], sys.argv[3], RandomBot())
+            print('Game finished: %d/%d' % (i+1, number_of_games))
     else:
         print("Usage: %s <server> <key> <[training|arena]> <number-of-games-to-play>" % (sys.argv[0]))
         print('Example: %s http://vindinium.jousse.org mySecretKey training 20' % (sys.argv[0]))
